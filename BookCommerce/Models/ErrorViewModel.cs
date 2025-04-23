551bb691fa1f601f00226d02f9f1e904c0830d5a
SisanKarki <sisankarki786@gmail.com>
2025-04-23 18:21:58 +0545

    Add project files.

---
+++ BookCommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCommerce.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
